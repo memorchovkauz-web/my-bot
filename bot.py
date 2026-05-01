6963505dad0a4da0b22875880d528f0fead8966c
memorchovkauz-web <memorchovkauz@gmail.com>
2026-05-01 18:18:02 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ import json
 from google.oauth2.service_account import Credentials
 
 creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-creds = Credentials.from_service_account_info(creds_dict)
+creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
 client = gspread.authorize(creds)
 sheet = client.open(SHEET_NAME)
 
