67a30836bcf149a9ec5bc92034298b06ea461f68
memorchovkauz-web <memorchovkauz@gmail.com>
2026-05-02 08:31:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1191,9 +1191,14 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"OK")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
 
 def run_server():
     port = int(os.environ.get("PORT", 10000))
