09f9d8fac5816b1d3783eaa78ebbbd5e757296de
memorchovkauz-web <memorchovkauz@gmail.com>
2026-05-04 16:01:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,6 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 USERS = {
     492894595: {"role": "director", "name": "Jahongir Ganiyev"},
-    1950294510: {"role": "mechanic", "name": "Механик исми"},
     1026372827: {"role": "mechanic", "name": "Пармонов Гиёс"},
     7421855968: {"role": "mechanic", "name": "{Холикулов Шехроз"},
     492894594: {"role": "technadzor", "name": "Jahongir Ganiyev"},
